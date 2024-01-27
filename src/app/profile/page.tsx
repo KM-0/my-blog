888860899f85d00adf4ef0bf57a032f5978927eb
Keita Maeda <laughlaughlife@outlook.jp>
2024-01-27 17:21:56 +0900

    feat:profileページの実装

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,63 @@
 export default function Home() {
-  return <div>profile</div>;
+  return (
+    <div className="mt-14 flex w-full flex-col justify-start md:mt-20 md:px-8 xl:w-10/12">
+      <h2 className="mb-4 animate-track-in-animation text-2xl font-bold md:text-4xl">
+        About.
+      </h2>
+      <div className="flex flex-col gap-y-6 mx-2">
+        <div>
+          <h3 className="mb-2 animate-track-in-animation text-xl font-bold md:text-3xl">
+            Me
+          </h3>
+          <ul className="animate-track-in-animation-item list-disc mx-8 flex flex-col gap-y-2">
+            <li>
+              名前
+              <br />
+              前田 啓太
+            </li>
+            <li>
+              生年月日
+              <br />
+              1996/7/15
+            </li>
+            <li>
+              出身地
+              <br />
+              神戸
+            </li>
+            <li>
+              GitHub
+              <br />
+              @KM-0
+            </li>
+            <li>
+              X
+              <br />
+              @keimae_dev
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="mb-2 animate-track-in-animation text-xl font-bold md:text-3xl">
+            Hobby
+          </h3>
+          <ul className="animate-track-in-animation-item list-disc mx-8 flex flex-col gap-y-2">
+            <li>漫画</li>
+            <li>映画鑑賞</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="mb-2 animate-track-in-animation text-xl font-bold md:text-3xl">
+            Skill
+          </h3>
+          <ul className="animate-track-in-animation-item list-disc mx-8 flex flex-col gap-y-2">
+            <li>Java</li>
+            <li>JSP</li>
+            <li>React</li>
+            <li>TypeScript</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
