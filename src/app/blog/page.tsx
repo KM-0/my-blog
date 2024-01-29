ab021682120687a721131f786f9fac4d2b5c8c1c
Keita Maeda <laughlaughlife@outlook.jp>
2024-01-29 23:26:14 +0900

    feat:BLOG一覧ページの追加

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>blog</div>;
+import { PostList } from "@/components/parts/PostList";
+import { client } from "@/lib/client";
+import { BlogType } from "@/types/blog";
+
+export default async function Home() {
+  const data = await client.getList<BlogType>({
+    endpoint: "blogs",
+  });
+
+  return (
+    <div className="mt-14 w-full md:mt-20 xl:w-10/12">
+      <div>
+        <h1 className="animate-track-in-animation text-3xl font-bold md:text-4xl">
+          Blog
+        </h1>
+      </div>
+      {data.contents.length === 0 ? (
+        <p className="font-bold text-gray-200">データがありません</p>
+      ) : (
+        <PostList data={data.contents} />
+      )}
+    </div>
+  );
 }
